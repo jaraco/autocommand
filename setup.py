ec6f6641c10be1e9285be5d31c96462f251889d6
Jason R. Coombs <jaraco@jaraco.com>
2024-04-04 17:23:20 -0400

    Remove license declaration and rely instead on Trove classifiers and LICENSE being included.
    Closes #32.

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     ],
     package_dir={'': 'src'},
     platforms='any',
-    license='LGPLv3',
     author='Nathan West',
     url='https://github.com/Lucretiel/autocommand',
     description='A library to create a command-line program from a function',
